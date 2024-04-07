4b6d35abb33b30f1fdf07ac7d82e9747212db2db
Pradeep Bashyal <pbashyal@nmdp.org>
2024-04-07 19:27:17 +0000

    Use macros in model stg_overall_conversion_rate.sql

--- greenery/macros/unique_purchases.sql
+++ greenery/macros/unique_purchases.sql
@@ -1,3 +1,6 @@
-{% macro unique_purchases() %}
-where EVENT_TYPE = 'checkout' group by SESSION_ID
+{% macro unique_purchases(event_table) %}
+
+select count(distinct SESSION_ID) as purchase_count
+                   from {{ ref(event_table) }}
+                    where EVENT_TYPE = 'checkout' group by SESSION_ID
 {% endmacro %}
--- greenery/models/staging/postgres/stg_overall_conversion_rate.sql
+++ greenery/models/staging/postgres/stg_overall_conversion_rate.sql
@@ -2,12 +2,8 @@
     config(materialized = 'view')
 }}
 
-with numerator as (select count(distinct SESSION_ID) as purchase_count
-                   from {{ ref("stg_postgres__events") }}
-                   {{ unique_purchases() }}),
-     denominator as (select count(*) as total_count
-                     from {{ ref("stg_postgres__events") }}
-                     group by SESSION_ID)
+with numerator as ({{ unique_purchases("stg_postgres__events") }}),
+     denominator as ({{ unique_sessions("stg_postgres__events")}})
 
 select {{ conversion_rate(purchase_count, total_count) }} as overall_conversion_rate
 from numerator, denominator
