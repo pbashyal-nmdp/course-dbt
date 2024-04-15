b0898c0e8335921410bb82f659b50ff158462cf9
Pradeep Bashyal <pbashyal@nmdp.org>
2024-04-15 03:54:24 +0000

    Added Checkout funnel

---
+++ greenery/models/staging/postgres/stg_checkout__sessions.sql
@@ -0,0 +1,10 @@
+{{
+    config(materialized = 'view')
+}}
+
+select events.PRODUCT_ID, products.name, count(distinct SESSION_ID) as views
+from {{ ref("stg_postgres__events") }} events
+        join {{ ref("stg_postgres__products") }} products
+            on events.PRODUCT_ID = products.PRODUCT_ID,
+where EVENT_TYPE = 'checkout'
+group by events.PRODUCT_ID, products.name, events.EVENT_TYPE
